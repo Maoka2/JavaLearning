421930dc130129889146a8e1ace5bd91123106af
Maoka2 <a24358232@gmail.com>
2025-02-08 18:44:20 +0900

    1874 스택수열 (Stack) https://www.acmicpc.net/problem/1874

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,48 +1,54 @@
 package BJPrc;
 
 import java.io.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int T = Integer.parseInt(br.readLine());
 
-        for(int tc = 0; tc < T; tc++){
+        int N = Integer.parseInt(br.readLine());
+        int[] stackNum = new int[N];
+        for(int i = 0; i < N; i++){
+            stackNum[i] = Integer.parseInt(br.readLine()); // 만들고싶은 수열
+        }
 
-            int N = Integer.parseInt(br.readLine());
+        Stack<Integer> numbers = new Stack<Integer>();
 
-            // 1 -> (1) -> 1
-            // 2 -> (1,1) , (2) -> 2
-            // 3 -> (1,1,1) , (1,2) , (2,1), (3) -> 4
-            // 4 -> (1,1,1,1),(1,1,2),(1,2,1),(2,1,1),(2,2),(1,3),(3,1) -> 7 (3만든거에 1), (2만든거에 2), (1 만든거에 3 == 3,1))
-            // 5 -> (4 만든것들에다 +1) / (3 만든거에다 2만든거), (2만든 거에다 3만든거)
+        int number = 1;
+        int index = 0;
+        while(number <= N || !numbers.isEmpty() ){
 
-            // N 번째 경우의 수 -> x_n = x_n-3 + x_n-2 + x_n-1
-            if(N == 1){
-                bw.write("1" + "\n");
-                continue;
-            } else if(N == 2){
-                bw.write("2" + "\n");
-                continue;
-            } else if(N == 3){
-                bw.write("4" + "\n");
-                continue;
-            } else {
-                int[] makeN = new int[11];
-
-                makeN[1] = 1;
-                makeN[2] = 2;
-                makeN[3] = 4;
-                for(int i = 4; i <= N; i++){
-                    makeN[i] = makeN[i-3] + makeN[i-2] + makeN[i-1];
-                }
-                bw.write(makeN[N] + "\n");
+            if(number <= N){
+                numbers.push(number);
+                number++;
+                bw.write("+" + "\n");
             }
 
 
+            while(!numbers.isEmpty() && numbers.peek() == stackNum[index]){
+                numbers.pop();
+                bw.write("-" + "\n");
+                index++;
+
+
+
+            }
+
+
+
+
+
         }
+
+        if(!numbers.isEmpty()){
+            bw.write("NO" + "\n");
+            return;
+        }
+
+
         bw.flush();
         bw.close();
         br.close();
