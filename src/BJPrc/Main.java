cf89e58daf7b84b423dfdbd0c04f4a670a802968
Maoka2 <a24358232@gmail.com>
2025-02-09 14:17:04 +0900

    백준 2609 https://www.acmicpc.net/problem/2609 오답수정 정답본 , 최소최대공약공배

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,43 +9,35 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String[] s = br.readLine().split(" ");
-        int[] n = new int[s.length];
-        for(int i = 0; i < n.length; i++){
-            n[i] = Integer.parseInt(s[i]);
-        }
-        int max = 0;
-        int min = 0;
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
         // 최소 공배수 , 최대 공약수?
         // 최소 공배수 -> 최대공약수 * 각 수 최대공약수로 나눈 몫
         // 최대 공약수는? 작은 수의 약수로 큰 수를 나누었을 때 나머지가 0 이 되는 놈들 중 가장 큰 거?
-        if(n[0] > n[1]){
-            max = n[0];
-            min = n[1];
-        } else if(n[0] < n[1]){
-            max = n[1];
-            min = n[0];
-        } else{
-            bw.write(n[0] + "\n");
-            bw.write(n[0] + "\n");
+        if(a == b){
+
+            bw.write(a + "\n");
+            bw.write(a + "\n");
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
         }
 
-        int a = max;
-        int b = min;
-        int remains = 1;
-        int temp = 1;
-        while(true){
-            remains = b;
-            temp = a % b;
-            b = temp;
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
 
-            if(temp == 0){
-                bw.write(remains + "\n");
-                break;
-            }
+        while(min != 0){
+            int d = max % min;
+            max = min;
+            min = d;
         }
 
 
-        int minMultiply = remains * (max / remains) * (min / remains);
+
+
+        int minMultiply = a*b/max;
+        bw.write(max + "\n");
         bw.write(minMultiply + "\n");
         bw.flush();
         bw.close();
