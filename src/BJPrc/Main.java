9fdb7c489e77aab1d7c961a7b243cd20ad4785ca
Maoka2 <a24358232@gmail.com>
2025-02-09 14:38:39 +0900

    백준 1874 출력 내용 수정 https://www.acmicpc.net/problem/1874

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,6 +1,7 @@
 package BJPrc;
 
 import java.io.*;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -8,41 +9,41 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] s = br.readLine().split(" ");
-        int a = Integer.parseInt(s[0]);
-        int b = Integer.parseInt(s[1]);
-        // 최소 공배수 , 최대 공약수?
-        // 최소 공배수 -> 최대공약수 * 각 수 최대공약수로 나눈 몫
-        // 최대 공약수는? 작은 수의 약수로 큰 수를 나누었을 때 나머지가 0 이 되는 놈들 중 가장 큰 거?
-        if(a == b){
-
-            bw.write(a + "\n");
-            bw.write(a + "\n");
-            bw.flush();
-            bw.close();
-            br.close();
-            return;
+        int N = Integer.parseInt(br.readLine());
+        int[] stackNum = new int[N];
+        for(int i = 0; i < N; i++){
+            stackNum[i] = Integer.parseInt(br.readLine()); // 만들고싶은 수열
         }
 
-        int max = Math.max(a,b);
-        int min = Math.min(a,b);
+        Stack<Integer> numbers = new Stack<Integer>();
+        StringBuilder sb =new StringBuilder();
+        int number = 1;
+        int index = 0;
+        while(index < N){
 
-        while(min != 0){
-            int d = max % min;
-            max = min;
-            min = d;
+            if(number <= stackNum[index]){
+                numbers.push(number++);
+                sb.append("+\n");
+
+            }else if(!numbers.isEmpty() && numbers.peek() == stackNum[index]){
+                numbers.pop();
+                sb.append("-\n");
+                index++;
+            }else{
+                bw.write("NO\n");
+                bw.flush();
+                return;
+            }
+
+
         }
 
 
 
-
-        int minMultiply = a*b/max;
-        bw.write(max + "\n");
-        bw.write(minMultiply + "\n");
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
 }
-
