9953f8d83ba9cfc0c34dd3f21237e3562216bf98
Maoka2 <a24358232@gmail.com>
2025-02-10 20:38:42 +0900

    백준 1158 요세푸스 https://www.acmicpc.net/problem/1158 스트링빌더, circular?

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,47 +8,32 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        String[] s = br.readLine().split(" ");
+        int n = Integer.parseInt(s[0]); // 주어지는 수들
+        int k = Integer.parseInt(s[1]);
 
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> treeList = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            treeList.add(Integer.parseInt(br.readLine()));
+        List<Integer> yosep = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            yosep.add(i);
         }
+        sb.append("<");
+        int index = 0;
+        while(!yosep.isEmpty()){
+            index = (index + k -1) % yosep.size();
+            sb.append(yosep.remove(index));
+            if(!yosep.isEmpty()){
+                sb.append(", ");
+            }
 
-        int[] interval = new int[N-1];
-        for (int i = 0; i < N - 1; i++) {
-            interval[i] = treeList.get(i+1) - treeList.get(i);
+
+
         }
-
-        int gcd = findMulti(interval);
-        // 각 나무 간격 얼마나 할 지 정했음
-
-        int count = (treeList.get(treeList.size()-1) - treeList.get(0))/gcd +1;
-
-
-
-
-        bw.write((count - treeList.size()) + "\n");
-
-
+        sb.append(">");
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
 
-    public static int GCD(int a, int b){
-        if(b == 0){
-            return a;
-        }
-        return GCD(b, a%b);
-    }
-
-    public static int findMulti(int[] arr){
-        int gcd = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            gcd = GCD(gcd,arr[i]);
-        }
-        return gcd;
-    }
 }
