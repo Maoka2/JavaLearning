7b90ba1fc8a6c4340cd7011c172ea6b645687442
Maoka2 <a24358232@gmail.com>
2025-02-10 17:46:54 +0900

    백준 2485 가로수 https://www.acmicpc.net/problem/2485 등차수열, 최대공약수

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,27 +9,46 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(true){
-            int count = 0;
-            String s = br.readLine();
-            String aeiou = "aeiouAEIOU";
-            if(s.equals("#")){
-                break;
-            } // #들어오면 끝내기
-            for(int i = 0; i <s.length(); i++){
-                if(aeiou.indexOf(s.charAt(i)) != -1){
-                    count++;
-                }
-            }
-            bw.write(count + "\n");
+        int N = Integer.parseInt(br.readLine());
+        List<Integer> treeList = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            treeList.add(Integer.parseInt(br.readLine()));
         }
 
+        int[] interval = new int[N-1];
+        for (int i = 0; i < N - 1; i++) {
+            interval[i] = treeList.get(i+1) - treeList.get(i);
+        }
+
+        int gcd = findMulti(interval);
+        // 각 나무 간격 얼마나 할 지 정했음
+
+        int count = (treeList.get(treeList.size()-1) - treeList.get(0))/gcd +1;
 
 
+
+
+        bw.write((count - treeList.size()) + "\n");
 
 
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    public static int GCD(int a, int b){
+        if(b == 0){
+            return a;
+        }
+        return GCD(b, a%b);
+    }
+
+    public static int findMulti(int[] arr){
+        int gcd = arr[0];
+        for(int i = 1; i < arr.length; i++){
+            gcd = GCD(gcd,arr[i]);
+        }
+        return gcd;
     }
 }
