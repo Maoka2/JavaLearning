75a6a6d065d239728cb7fcde72fd7a2ab7aa9bfe
Maoka2 <a24358232@gmail.com>
2025-02-11 10:50:13 +0900

    백준 1463 1로 만들기 https://www.acmicpc.net/problem/1463 DP, 봤던 문제라 푼 듯?

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,39 +1,42 @@
 package BJPrc;
 
-import java.util.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-        String[] s = br.readLine().split(" ");
-        int n = Integer.parseInt(s[0]); // 주어지는 수들
-        int k = Integer.parseInt(s[1]);
 
-        List<Integer> yosep = new ArrayList<>();
-        for(int i = 1; i <= n; i++){
-            yosep.add(i);
-        }
-        sb.append("<");
-        int index = 0;
-        while(!yosep.isEmpty()){
-            index = (index + k -1) % yosep.size();
-            sb.append(yosep.remove(index));
-            if(!yosep.isEmpty()){
-                sb.append(", ");
+        int N = Integer.parseInt(br.readLine());
+
+        // 3으로 나누는게 제일 빨리 줄어들잖음
+        // 그다음에 2
+        // 3번은 언제 할까? -> 1을 빼서 3으로 나눌수있게 할때 쓰면 좋을것같은데
+        // 3으로 나눠지는지 어떻게 아냐? -> 각 자리 수 합이 3으로 나눠지면 3의 배수
+
+        int[] minimum = new int[N+1];
+
+        for(int i = 2; i <=N; i++){
+            // 0, 1은 어차피 0이니까
+            minimum[i] = minimum[i-1] +1; // 일단 기본설정은 나에서 -1하면 내 이전수 라는 접근
+
+            if(i % 2 == 0){
+                minimum[i] = Math.min(minimum[i] , minimum[i/2]+1);
             }
 
-
-
+            if(i % 3 == 0){
+                minimum[i] = Math.min(minimum[i] , minimum[i/3] +1);
+            }
         }
-        sb.append(">");
-        bw.write(sb.toString());
+
+        bw.write(minimum[N] + "\n");
+
         bw.flush();
         bw.close();
         br.close();
     }
+
 
 }
