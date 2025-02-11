ce3489bf07473e5b92a1aa094cef99f7708fbb5e
Maoka2 <a24358232@gmail.com>
2025-02-11 17:51:26 +0900

    백준 10773 제로 https://www.acmicpc.net/problem/10773 스택활용 push pop

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,89 +1,37 @@
 package BJPrc;
-
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Arrays;
-import java.util.Deque;
 import java.util.Stack;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Deque<Integer> deque = new ArrayDeque<>();
-
-        int N = Integer.parseInt(br.readLine());
-        int head = 0;
-        int tail = -1;
-        int size = 0;
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-
-            switch (s[0]) {
-
-                case "push_back":
-                    deque.offer(Integer.parseInt(s[1]));
-                    break;
-
-                case "push_front":
-                    deque.push(Integer.parseInt(s[1]));
-                    break;
-
-                case "pop_front":
-                    if (deque.isEmpty()) {
-                        bw.write(-1 + "\n");
-                        break;
+            int N = Integer.parseInt(br.readLine());
+            Stack<Integer> numStack = new Stack<>();
+            for(int i = 0; i < N; i++){
+                int n = Integer.parseInt(br.readLine());
+                if(n != 0){
+                    numStack.push(n);
+                } else{
+                    if(!numStack.isEmpty()){ // 보장한다고하긴함
+                        numStack.pop();
                     }
-                    bw.write(deque.pollFirst() + "\n");
-                    break;
-
-                case "pop_back":
-                    if (deque.isEmpty()) {
-                        bw.write(-1 + "\n");
-                        break;
-                    }
-                    bw.write(deque.pollLast() + "\n");
-                    break;
-
-                case "size":
-                    bw.write(deque.size() + "\n");
-                    break;
-
-                case "empty":
-                    if (deque.isEmpty()) {
-                        bw.write(1 + "\n");
-                    } else {
-                        bw.write(0 + "\n");
-                    }
-                    break;
-
-                case "front":
-                    if (deque.isEmpty()) {
-                        bw.write(-1 + "\n");
-                        break;
-                    } else {
-                        bw.write(deque.peekFirst() + "\n");
-                        break;
-                    }
-                case "back":
-                    if(deque.isEmpty()){
-                        bw.write(-1 + "\n");
-                        break;
-                    } else{
-                        bw.write(deque.peekLast() + "\n");
-                        break;
-                    }
+                }
+            }
+            int sum = 0;
+            for(int num : numStack){
+                sum += num;
             }
 
-        }
+        bw.write(sum + "\n");
 
         bw.flush();
-        bw.close();
         br.close();
+        bw.close();
+
+
     }
-
-
 }
-
