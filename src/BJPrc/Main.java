c7d5639e65aa843ce69906ec460f3f8e90efb67d
Maoka2 <a24358232@gmail.com>
2025-02-11 15:06:02 +0900

    백준 10866 덱 구현 https://www.acmicpc.net/problem/10866 맨첫, 맨뒤는 pollFirst,pollLast 와 같은 식으로 접근 가능

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,7 +1,10 @@
 package BJPrc;
 
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,42 +12,72 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        Deque<Integer> deque = new ArrayDeque<>();
+
         int N = Integer.parseInt(br.readLine());
-        int[] stepScore = new int[N+1];
-        for(int i = 1; i <= N; i++){
-            String s = br.readLine();
-            stepScore[i] = Integer.parseInt(s);
+        int head = 0;
+        int tail = -1;
+        int size = 0;
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+
+            switch (s[0]) {
+
+                case "push_back":
+                    deque.offer(Integer.parseInt(s[1]));
+                    break;
+
+                case "push_front":
+                    deque.push(Integer.parseInt(s[1]));
+                    break;
+
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        bw.write(-1 + "\n");
+                        break;
+                    }
+                    bw.write(deque.pollFirst() + "\n");
+                    break;
+
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        bw.write(-1 + "\n");
+                        break;
+                    }
+                    bw.write(deque.pollLast() + "\n");
+                    break;
+
+                case "size":
+                    bw.write(deque.size() + "\n");
+                    break;
+
+                case "empty":
+                    if (deque.isEmpty()) {
+                        bw.write(1 + "\n");
+                    } else {
+                        bw.write(0 + "\n");
+                    }
+                    break;
+
+                case "front":
+                    if (deque.isEmpty()) {
+                        bw.write(-1 + "\n");
+                        break;
+                    } else {
+                        bw.write(deque.peekFirst() + "\n");
+                        break;
+                    }
+                case "back":
+                    if(deque.isEmpty()){
+                        bw.write(-1 + "\n");
+                        break;
+                    } else{
+                        bw.write(deque.peekLast() + "\n");
+                        break;
+                    }
+            }
+
         }
-        int[] sum = new int[N+1];
-
-        if(N==1){
-            bw.write(stepScore[1] + "\n");
-            bw.flush();
-            bw.close();
-            br.close();
-            return;
-        }
-        sum[1] = stepScore[1];
-
-        sum[2] = stepScore[1] + stepScore[2];
-        if(N == 2){
-            bw.write(sum[2] + "\n");
-            bw.flush();
-            bw.close();
-            br.close();
-            return;
-        }
-
-        sum[3] = Math.max(stepScore[1],stepScore[2]) + stepScore[3];
-        // 마지막 계단은 무적권 밟아야 하니까
-        // case1. N-2번째 계단에서 2개 연속으로 밟기
-        // case2. N-1번째 계단에서 그냥 하나 밟고 끝
-
-        for(int i = 4; i <=N; i++){
-            sum[i] = Math.max(sum[i-2],sum[i-3]+stepScore[i-1]) + stepScore[i];
-        }
-
-        bw.write(sum[N] + "\n");
 
         bw.flush();
         bw.close();
@@ -53,3 +86,4 @@ public class Main {
 
 
 }
+
