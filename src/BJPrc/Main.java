335296e9b55a724e682fd4e8c1343d2728b7fa9c
Maoka2 <a24358232@gmail.com>
2025-02-15 18:55:29 +0900

    백준 2309 일곱난쟁이 IM대비
    https://www.acmicpc.net/problem/2309
    제거해야 되는게 더 적으면 뭘 뺼지 생각해보기

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,7 +1,8 @@
 package BJPrc;
+
 import java.io.*;
 import java.util.Arrays;
-import java.util.Stack;
+import java.util.Collections;
 
 public class Main {
 
@@ -10,46 +11,36 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 남학생은 남학생끼리, 여는 여끼리
-        // 같은 학년끼리 , 혼자쓰는것도 가능
-        String[] NK = br.readLine().split(" ");
-        int N = Integer.parseInt(NK[0]); // 학생인원
-        int K = Integer.parseInt(NK[1]); // 방 최대인원수
-        // 여 0 , 남 1
-        // 0번인덱스 : 학년, 1번인덱스 : 성별
-
-        int[][] student = new int[6][2];  // 남자면 1번 인덱스에 넣을거고 여자면 0 번 인덱스에 넣을거임
-
-        for(int i = 0; i < N; i++){
-            String[] s = br.readLine().split(" ");
-            int MFM = 0;
-            int grade = 0;
-            if(Integer.parseInt(s[0]) == 1){
-                MFM = 1;
-            } else if(Integer.parseInt(s[0]) == 0){
-                MFM = 0;
-            }
-            grade = Integer.parseInt(s[1])-1;
-
-            student[grade][MFM]++;
+        int[] heights = new int[9];
+        for (int i = 0; i < 9; i++) {
+            heights[i] = Integer.parseInt(br.readLine());
+        }
+        int sum = 0;
+        Arrays.sort(heights);
+        for(int i = 0; i < 9; i++){
+            sum += heights[i];
         }
 
-        int count = 0;
-        for(int i = 0; i < 6; i++){
-            for(int j = 0; j < 2; j++){
-                if(student[i][j] <= K && student[i][j] > 0){
-                    count++;
-                } else if (student[i][j] > K){
-                    if(student[i][j] % K == 0){
-                        count += student[i][j] / K;
-                    } else{
-                        count += student[i][j] / K + 1;
-                    }
+        // 다 더해서 일단 다 빼면서  100일 때 break;
+        int minus1 = 0;
+        int minus2 = 0;
+        a:
+        for(int i = 0; i <= 7; i++){
+            for(int j = i+1; j < 9; j++){
+                if(sum - heights[i] - heights[j] == 100){
+                    minus1 = heights[i];
+                    minus2 = heights[j];
+                    break a;
                 }
             }
         }
 
-        bw. write(count + "\n");
+        for(int i = 0; i  < 9; i++){
+            if(heights[i] != minus1 && heights[i] != minus2){
+                bw.write(heights[i] + "\n");
+            }
+        }
+
         bw.flush();
         br.close();
         bw.close();
