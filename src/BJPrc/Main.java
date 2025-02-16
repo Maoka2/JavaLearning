f1dce758d5bd95ea9c0b93bf4e8f03706e3cd604
Maoka2 <a24358232@gmail.com>
2025-02-16 15:08:40 +0900

    백준 14696 딱지놀이 https://www.acmicpc.net/problem/14696 Map, getOrDefault, Null 주의 하기

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,10 +1,7 @@
 package BJPrc;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -12,17 +9,54 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int N = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(br.readLine()); // 라운드 수
+
+        for (int tc = 0; tc < N; tc++) {
+            Map<Integer, Integer> aMap = new HashMap<>();
+            Map<Integer, Integer> bMap = new HashMap<>();
+
+            String[] sa = br.readLine().split(" ");
+            int[] ia = new int[sa.length - 1];
+            for (int i = 1; i < sa.length; i++) {
+                ia[i - 1] = Integer.parseInt(sa[i]);
+            }
+
+            boolean draw = true;
+
+            for (int i = 0; i < ia.length; i++) {
+                aMap.put(ia[i], aMap.getOrDefault(ia[i], 0) + 1);
+            }
+
+            String[] sb = br.readLine().split(" ");
+            int[] ib = new int[sb.length - 1];
+            for (int i = 1; i < sb.length; i++) {
+                ib[i - 1] = Integer.parseInt(sb[i]);
+            }
+
+            for (int i = 0; i < ib.length; i++) {
+                bMap.put(ib[i], bMap.getOrDefault(ib[i], 0) + 1);
+            }
+
+            for (int i = 4; i >= 1; i--) {
+                if (aMap.getOrDefault(i, 0) > bMap.getOrDefault(i, 0)) {
+                    bw.write("A\n");
+                    draw = false;
+                    break;
+                } else if (bMap.getOrDefault(i, 0) > aMap.getOrDefault(i, 0)) {
+                    bw.write("B\n");
+                    draw = false;
+                    break;
+                }
+            }
+            if (draw) {
+                bw.write("D\n");
+            }
+
+            // 규칙 : 우선순위 : 별 > 동그라미 > 네모 > 세모
+            // 숫자 다 같으면 무승부
+            // 별 : 4 동그라미 : 3 네모 : 2 세모 : 1
 
 
-        // 대충 뽑은 번호 수 만큼 앞으로 갈 수 있음
-        List<Integer> l = new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            l.add(i-Integer.parseInt(s[i]), i+1);
-        }
-        for(int num : l){
-            bw. write(num + " ");
         }
 
 
