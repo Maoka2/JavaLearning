8d0e55d80395a8490d3e6fc3c4feeb9badd1ffbe
Maoka2 <a24358232@gmail.com>
2025-02-19 20:39:07 +0900

    백준 2493 탑 https://www.acmicpc.net/problem/2493 스택, 인덱스로 접근

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,68 +1,58 @@
 package BJPrc;
 
 import java.io.*;
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int max = -1;
+        int maxIdx = -1;
+        Stack<Integer> towers = new Stack<>();
+        int[] temp = new int[N];
+        for (int i = 0; i < N; i++) {
+            temp[i] = Integer.parseInt(s[i]);
+        }
+        int[] result = new int[N];
 
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 0; tc < T; tc++) {
-            Deque<Integer> deque = new ArrayDeque<>();
-            String p = br.readLine();
-            int n = Integer.parseInt(br.readLine());
-            String strX = br.readLine();
-            if (n > 0) {
-                String[] strArrayX = strX.substring(1, strX.length() - 1).split(",");
-                for (int i = 0; i < strArrayX.length; i++) {
-                    deque.add(Integer.parseInt(strArrayX[i]));
-                }
+        for (int i = 0; i < N; i++) {
+            while (!towers.isEmpty() && temp[i] > temp[towers.peek()]) {
+                towers.pop();
             }
-            boolean isError = false;
-            boolean isReversed = false;
 
-            for (int i = 0; i < p.length(); i++) {
-                if (p.charAt(i) == 'R') {
-                    isReversed = !isReversed;
-                    // 뒤집어져있지 않으면? 그냥 앞에서 빼버리고
-                    // 아니면? 뒤에서 빼버리기
-                    // 메모리 관점? 새로운 덱 만드는게 좋아보이진 않아서 이제
-
-                } else if (p.charAt(i) == 'D') {
-                    if (deque.isEmpty()) {
-                        isError = true;
-                        bw.write("error\n");
-                        break;
-                    }
-                    // reverse T/F 에 따라서 이제 bw pollFirst / pollLast 할건지 그것만 해주면 될것같다.
-                    if (isReversed) {
-                        deque.pollLast();
-                    } else {
-                        deque.pollFirst();
-                    }
-                }
+            if (towers.isEmpty()) {
+                result[i] = 0;
+            } else {
+                result[i] = towers.peek() + 1;
             }
-            // 에러면 굳이 뭐 건드릴필요없고, 에러가 아닌경우에만 출력을 해주면 되겠다.
-            if (!isError) {
-                bw.write("[");
 
-                int fSize = deque.size();
-                for (int i = 0; i < fSize; i++) {
-                    if (isReversed) {
-                        bw.write(String.valueOf(deque.pollLast()));
-                    } else {
-                        bw.write(String.valueOf(deque.pollFirst()));
-                    }
-                    if(i < fSize -1){
-                        bw.write(",");
-                    }
-                }
-                bw.write("]\n");
+            towers.push(i);
+        }
+
+        boolean isZeros = true;
+
+        for (int n : result) {
+            if (n != 0) {
+                isZeros = false;
             }
         }
+
+        if (isZeros) {
+            sb.append(0);
+        } else {
+            for (int i = 0; i < N; i++) {
+                sb.append(result[i]).append(" ");
+            }
+        }
+
+        String ans = sb.toString().trim();
+
+        bw.write(ans + "\n");
+
         bw.flush();
         bw.close();
         br.close();
