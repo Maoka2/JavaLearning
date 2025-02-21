9b29feee1b267438e982f42bc5441ea36a60c320
Maoka2 <a24358232@gmail.com>
2025-02-21 15:25:14 +0900

    백준 11866 요세푸스 문제0 https://www.acmicpc.net/problem/11866 어레이리스트, remove하면 사이즈가 줄어드는거 반영해야

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -5,25 +5,34 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        LinkedList<Integer> yosep = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        String[] nk = br.readLine().split(" ");
+        int N = Integer.parseInt(nk[0]);
+        int K = Integer.parseInt(nk[1]);
 
-
-
-        Deque<Integer> cards = new ArrayDeque<>();
-        int N = Integer.parseInt(br.readLine());
         for(int i = 1; i <= N; i++){
-            cards.add(i);
+            yosep.add(i);
         }
-        int initSize = cards.size();
-        while(initSize > 1){
-            cards.pollFirst();
-            cards.add(cards.pollFirst());
-            initSize--;
-        }
-        bw.write(cards.peekFirst() + "\n");
+        int idx = 0;
+        int size = N;
+        for(int i = 0; i < N; i++){
+            idx = (idx+K-1) % size;
+            size--;
+            if(i < N-1){
+                sb.append(yosep.remove(idx)).append(",").append(" ");
+            } else if(i == N-1){
+                sb.append(yosep.remove(idx)).append(">");
 
+            }
+
+        }
+
+        bw.write(sb + "\n");
+        sb.setLength(0);
         bw.flush();
         bw.close();
         br.close();
