0e400c0e079f5b798d21bd2292a250affb121d79
Maoka2 <a24358232@gmail.com>
2025-02-21 15:07:27 +0900

    백준 2164 카드2 https://www.acmicpc.net/problem/2164 Deque

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,28 +9,20 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] s = br.readLine().split(" ");
-        int[] size = new int[s.length];
-        for (int i = 0; i < s.length; i++) {
-            size[i] = Integer.parseInt(s[i]);
-        }
-        String[] ss = br.readLine().split(" ");
-        int T = Integer.parseInt(ss[0]);
-        int P = Integer.parseInt(ss[1]);
-        // T -> 각 나누기 5 몫 + 1
-        //펜 -> N / P, N % P
 
-        int tCount = 0;
-        for (int i = 0; i < s.length; i++) {
-            if (size[i] % T != 0) {
-                tCount += (size[i] / T) + 1;
-            } else{
-                tCount += size[i] / T;
-            }
+
+        Deque<Integer> cards = new ArrayDeque<>();
+        int N = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= N; i++){
+            cards.add(i);
         }
-        bw.write(tCount + "\n");
-        bw.write(N / P + " " + N % P + "\n");
+        int initSize = cards.size();
+        while(initSize > 1){
+            cards.pollFirst();
+            cards.add(cards.pollFirst());
+            initSize--;
+        }
+        bw.write(cards.peekFirst() + "\n");
 
         bw.flush();
         bw.close();
