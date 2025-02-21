4135f64eb82319aba9ec5a8f0a3b6939ef4d8d7a
Maoka2 <a24358232@gmail.com>
2025-02-21 15:40:28 +0900

    백준 1436 영화감독 숌 https://www.acmicpc.net/problem/1436 int -> String 비교

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -7,32 +7,19 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        LinkedList<Integer> yosep = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        String[] nk = br.readLine().split(" ");
-        int N = Integer.parseInt(nk[0]);
-        int K = Integer.parseInt(nk[1]);
 
-        for(int i = 1; i <= N; i++){
-            yosep.add(i);
-        }
-        int idx = 0;
-        int size = N;
-        for(int i = 0; i < N; i++){
-            idx = (idx+K-1) % size;
-            size--;
-            if(i < N-1){
-                sb.append(yosep.remove(idx)).append(",").append(" ");
-            } else if(i == N-1){
-                sb.append(yosep.remove(idx)).append(">");
-
+        int N = Integer.parseInt(br.readLine());
+        // 6667 이런것도 종말의 수?
+        int count = 0;
+        int num = 0;
+        while(count != N){
+            num++;
+            if(String.valueOf(num).contains("666")){
+                count++;
             }
-
         }
+        bw.write(num + "\n");
 
-        bw.write(sb + "\n");
-        sb.setLength(0);
         bw.flush();
         bw.close();
         br.close();
