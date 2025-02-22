9718d70d630a2b0fa1da9cc01effbd53908c75db
Maoka2 <a24358232@gmail.com>
2025-02-22 14:49:58 +0900

    백준 10814 나이순 정렬 https://www.acmicpc.net/problem/10814 익명함수 comparator

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,20 +8,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int K = Integer.parseInt(s[1]);
-        int result = 1;
-        int divide = 1;
-        int count = K;
-        for(int i = 0; i < count; i++){
-            result *= N;
-            divide *= K;
-            N--;
-            K--;
+        int N = Integer.parseInt(br.readLine());
+        String[][] members = new String[N][2];
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            members[i][0] = s[0];
+            members[i][1] = s[1];
         }
 
-        bw.write(result/divide + "\n");
+        Arrays.sort(members, new Comparator<String[]>(){
+
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            bw.write(members[i][0] + " " + members[i][1] + "\n");
+
+
+        }
+
 
         bw.flush();
         bw.close();
