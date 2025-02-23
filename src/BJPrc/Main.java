35532bfaf0e8a4d88085b62be49da284e00638e5
Maoka2 <a24358232@gmail.com>
2025-02-23 22:05:31 +0900

    백준 18110 솔브닷 https://www.acmicpc.net/problem/18110

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,40 +8,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String s;
-        while (true) {
-            s = br.readLine();
-            if (s.equals(".")) {
-                break;
-            }
-            Stack<Character> stack = new Stack<>();
-            boolean possible = true;
-            for (int i = 0; i < s.length(); i++) {
-                if (s.charAt(i) == '(' ||  s.charAt(i) == '[') {
-                    stack.push(s.charAt(i));
-                } else if (s.charAt(i) == ')' ||  s.charAt(i) == ']') {
-                    if (stack.isEmpty()) {
-                        possible = false;
-                        break;
-                    }
-                    if (s.charAt(i) == ')' && stack.peek() == '(' ||  s.charAt(i) == ']' && stack.peek() == '[') {
-                        stack.pop();
-                    } else {
-                        possible = false;
-                        break;
-                    }
-                }
-            }
-            if(!stack.isEmpty()){
-                possible = false;
-            }
-
-            if(possible){
-                bw.write("yes\n");
-            } else{
-                bw.write("no\n");
-            }
+        int N = Integer.parseInt(br.readLine());
+        if(N == 0){
+            bw.write("0\n");
+            bw.flush();
+            bw.close();
+            br.close();
+            return;
         }
+        int[] difficulty = new int[N];
+        for(int i = 0; i < N; i++){
+            difficulty[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(difficulty);
+        int range = Math.round((float) (N * 15) / 100);
+        int sum = 0;
+        int member = N - range * 2;
+        for(int i = range; i < N-range; i++){
+            sum+= difficulty[i];
+        }
+
+        int ans = Math.round((float) sum / member);
+        bw.write(ans + "\n");
         bw.flush();
         bw.close();
         br.close();
