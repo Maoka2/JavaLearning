9120400e0062b3a03cf969c9068244eab7fee7f0
Maoka2 <a24358232@gmail.com>
2025-02-23 23:56:02 +0900

    백준 11723 집합 https://www.acmicpc.net/problem/11723

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,39 +8,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] s = br.readLine().split(" ");
-        int K = Integer.parseInt(s[0]);
-        int N = Integer.parseInt(s[1]);
-        long[] lines = new long[K];
-        for (int i = 0; i < K; i++) {
-            lines[i] = Long.parseLong(br.readLine());
-        }
-        // 802 / ans  + 743 /ans + 457 / ans + 539 / ans -> 11 이 되게 하는 ans의 최대값
+        int M = Integer.parseInt(br.readLine());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < M; i++) {
+            String[] s = br.readLine().split(" ");
 
-        //냅다 그냥 다 찾거나
-
-        long left = 1;
-        Arrays.sort(lines);
-        long right = lines[K - 1];
-        long temp = 0;
-        while (left <= right) {
-            int count = 0;
-            long center = (left + right) / 2;
-
-            for(int i = 0; i < K; i++){
-                count += lines[i] / center;
+            if (s[0].equals("add")) {
+                set.add(Integer.parseInt(s[1]));
+            } else if (s[0].equals("check")) {
+                if (set.contains(Integer.parseInt(s[1]))) {
+                    bw.write(1 + "\n");
+                } else {
+                    bw.write(0 + "\n");
+                }
+            } else if (s[0].equals("remove")) {
+                set.remove(Integer.parseInt(s[1]));
+            } else if (s[0].equals("toggle")) {
+                if (set.contains(Integer.parseInt(s[1]))) {
+                    set.remove(Integer.parseInt(s[1]));
+                } else {
+                    set.add(Integer.parseInt(s[1]));
+                }
+            } else if (s[0].equals("all")) {
+                for (int j = 1; j <= 20; j++) {
+                    set.add(j);
+                }
+            } else if (s[0].equals("empty")) {
+                set.clear();
             }
 
-            if(count < N){
-                // 덜 만들어졌으면 center 값이 작아져야
-                right = center-1;
-            }else{
-                temp = center;
-                left = center + 1;
-            }
-        }
 
-        bw.write(temp + "\n");
+        }
 
         bw.flush();
         bw.close();
