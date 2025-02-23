4c5de772d6ad18d32665d652dba51ea5e7b3f5dc
Maoka2 <a24358232@gmail.com>
2025-02-23 15:36:06 +0900

    백준 28702 FizzBuzz https://www.acmicpc.net/problem/28702 switch 대신 메서드구현 했어도 괜찮았을듯

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,34 +1,71 @@
 package BJPrc;
 
-import java.util.*;
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        int N = Integer.parseInt(br.readLine());
-        int[][] wh = new int[N][2];
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-            wh[i][0] = Integer.parseInt(s[0]);
-            wh[i][1] = Integer.parseInt(s[1]);
-        }
-
-
-        for (int i = 0; i < N; i++) {
-            int k = 0;
-            for (int j = 0; j < N; j++) {
-                if (wh[i][0] < wh[j][0] && wh[i][1] < wh[j][1]) {
-                    k++;
+        String[] fb = new String[3];
+        int max = -1;
+        int idx = 0;
+        for (int i = 0; i < 3; i++) {
+            fb[i] = br.readLine();
+            if (!fb[i].equals("Fizz") && !fb[i].equals("Buzz") && !fb[i].equals("FizzBuzz")) {
+                if (Integer.parseInt(fb[i]) > max) {
+                    max = Integer.parseInt(fb[i]);
+                    idx = i;
                 }
             }
-            bw.write((k+1) + " ");
         }
+
+        switch (idx) {
+
+            case 0:
+                max += 3;
+                if (max % 15 == 0) {
+                    bw.write("FizzBuzz\n");
+                } else if (max % 5 == 0) {
+                    bw.write("Buzz");
+                } else if (max % 3 == 0) {
+                    bw.write("Fizz");
+                } else {
+                    bw.write(max + "\n");
+                }
+                break;
+
+            case 1:
+                max += 2;
+                if (max % 15 == 0) {
+                    bw.write("FizzBuzz\n");
+                } else if (max % 5 == 0) {
+                    bw.write("Buzz");
+                } else if (max % 3 == 0) {
+                    bw.write("Fizz");
+                } else {
+                    bw.write(max + "\n");
+                }
+                break;
+
+            case 2:
+                max += 1;
+                if (max % 15 == 0) {
+                    bw.write("FizzBuzz\n");
+                } else if (max % 5 == 0) {
+                    bw.write("Buzz");
+                } else if (max % 3 == 0) {
+                    bw.write("Fizz");
+                } else {
+                    bw.write(max + "\n");
+                }
+                break;
+        }
+
 
         bw.flush();
         bw.close();
         br.close();
+
     }
 }
