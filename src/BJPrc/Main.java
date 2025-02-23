e1d20113a8ebefafd6153cd9c83b5a86271b23ce
Maoka2 <a24358232@gmail.com>
2025-02-23 16:09:11 +0900

    백준 4949 균형잡힌 세상 https://www.acmicpc.net/problem/4949 스택

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,71 +1,50 @@
 package BJPrc;
 
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] fb = new String[3];
-        int max = -1;
-        int idx = 0;
-        for (int i = 0; i < 3; i++) {
-            fb[i] = br.readLine();
-            if (!fb[i].equals("Fizz") && !fb[i].equals("Buzz") && !fb[i].equals("FizzBuzz")) {
-                if (Integer.parseInt(fb[i]) > max) {
-                    max = Integer.parseInt(fb[i]);
-                    idx = i;
+
+        String s;
+        while (true) {
+            s = br.readLine();
+            if (s.equals(".")) {
+                break;
+            }
+            Stack<Character> stack = new Stack<>();
+            boolean possible = true;
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == '(' ||  s.charAt(i) == '[') {
+                    stack.push(s.charAt(i));
+                } else if (s.charAt(i) == ')' ||  s.charAt(i) == ']') {
+                    if (stack.isEmpty()) {
+                        possible = false;
+                        break;
+                    }
+                    if (s.charAt(i) == ')' && stack.peek() == '(' ||  s.charAt(i) == ']' && stack.peek() == '[') {
+                        stack.pop();
+                    } else {
+                        possible = false;
+                        break;
+                    }
                 }
             }
+            if(!stack.isEmpty()){
+                possible = false;
+            }
+
+            if(possible){
+                bw.write("yes\n");
+            } else{
+                bw.write("no\n");
+            }
         }
-
-        switch (idx) {
-
-            case 0:
-                max += 3;
-                if (max % 15 == 0) {
-                    bw.write("FizzBuzz\n");
-                } else if (max % 5 == 0) {
-                    bw.write("Buzz");
-                } else if (max % 3 == 0) {
-                    bw.write("Fizz");
-                } else {
-                    bw.write(max + "\n");
-                }
-                break;
-
-            case 1:
-                max += 2;
-                if (max % 15 == 0) {
-                    bw.write("FizzBuzz\n");
-                } else if (max % 5 == 0) {
-                    bw.write("Buzz");
-                } else if (max % 3 == 0) {
-                    bw.write("Fizz");
-                } else {
-                    bw.write(max + "\n");
-                }
-                break;
-
-            case 2:
-                max += 1;
-                if (max % 15 == 0) {
-                    bw.write("FizzBuzz\n");
-                } else if (max % 5 == 0) {
-                    bw.write("Buzz");
-                } else if (max % 3 == 0) {
-                    bw.write("Fizz");
-                } else {
-                    bw.write(max + "\n");
-                }
-                break;
-        }
-
-
         bw.flush();
         bw.close();
         br.close();
-
     }
 }
+
