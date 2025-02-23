4af2ab1456fbb01a3dd8f0dd163498b4cfb4ceec
Maoka2 <a24358232@gmail.com>
2025-02-23 22:51:07 +0900

    백준 1966 프린터큐 https://www.acmicpc.net/problem/1966 덱의 <> 안에 배열을 넣어도 된다!

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,31 +8,48 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        if(N == 0){
-            bw.write("0\n");
-            bw.flush();
-            bw.close();
-            br.close();
-            return;
-        }
-        int[] difficulty = new int[N];
-        for(int i = 0; i < N; i++){
-            difficulty[i] = Integer.parseInt(br.readLine());
-        }
-        Arrays.sort(difficulty);
-        int range = Math.round((float) (N * 15) / 100);
-        int sum = 0;
-        int member = N - range * 2;
-        for(int i = range; i < N-range; i++){
-            sum+= difficulty[i];
-        }
+        int T = Integer.parseInt(br.readLine());
+        for(int i = 0; i < T; i++){
+            Deque<int[]> q = new ArrayDeque<>();
+            String[] s = br.readLine().split(" ");
+            int N = Integer.parseInt(s[0]);
+            int M = Integer.parseInt(s[1]);
 
-        int ans = Math.round((float) sum / member);
-        bw.write(ans + "\n");
+            String[] pri = br.readLine().split(" ");
+            for(int j = 0; j < N; j++){
+                q.add(new int[]{Integer.parseInt(pri[j]), j});
+            }
+
+            int count = 0;
+
+            while(!q.isEmpty()){
+                int[] current = q.pollFirst();
+                int priority = current[0];
+                int idx = current[1];
+                boolean isHigher = false;
+                // 우선순위가 더 높은게 있다면 current 맨 뒤로 보내기
+                for(int[] n : q){
+                    if(n[0] > priority){
+                        isHigher = true;
+                        break;
+                    }
+                }
+
+                if(isHigher){
+                    q.addLast(current);
+                } else{
+                    count++;
+                    if(idx == M){
+                        bw.write(count + "\n");
+                        break;
+                    }
+                }
+            }
+
+
+        }
         bw.flush();
         bw.close();
         br.close();
     }
 }
-
