9db3beb34989e22519d78dd6037de8f517529cb3
Maoka2 <a24358232@gmail.com>
2025-02-25 17:43:43 +0900

    백준 9375 패션왕 신해빈 https://www.acmicpc.net/problem/9375 수학? 경우의수?

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,20 +9,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] s = br.readLine().split(" ");
-        int N = Integer.parseInt(s[0]);
-        int M = Integer.parseInt(s[1]);
+        int T = Integer.parseInt(br.readLine());
 
-        Map<String,String> m = new HashMap<>();
+        for (int i = 0; i < T; i++) {
+            Map<String, Integer> m = new HashMap<>();
+            int n = Integer.parseInt(br.readLine());
+            for (int j = 0; j < n; j++) {
+                String[] s = br.readLine().split(" ");
+                m.put(s[1], m.getOrDefault(s[1], 0) + 1);
+            }
+            int sum = 1;
 
-        for(int i = 0; i < N; i++){
-            String[] pw = br.readLine().split(" ");
-            m.put(pw[0],pw[1]);
-        }
+            for(String str : m.keySet()){
+                sum = sum * (m.get(str)+1);
+            }
 
-        for(int i = 0; i < M; i++){
-            String ss = br.readLine();
-            bw.write(m.get(ss) + "\n");
+            bw.write(sum-1 + "\n");
         }
         bw.flush();
         br.close();
