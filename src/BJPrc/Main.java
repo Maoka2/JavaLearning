360d68caf8c8c08fae39bd12c9820f8696a84dcb
Maoka2 <a24358232@gmail.com>
2025-02-25 17:13:34 +0900

    백준 11047 동전0 https://www.acmicpc.net/problem/11047 그리디

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,35 +9,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int computer = Integer.parseInt(br.readLine());
-        int lines = Integer.parseInt(br.readLine());
-        List<List<Integer>> l = new ArrayList<>();
-        for (int i = 0; i <= computer; i++) {
-            l.add(new ArrayList<>());
-        }
-        for (int i = 0; i < lines; i++) {
-            String[] s = br.readLine().split(" ");
-            l.get(Integer.parseInt(s[0])).add(Integer.parseInt(s[1]));
-            l.get(Integer.parseInt(s[1])).add(Integer.parseInt(s[0]));
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int K = Integer.parseInt(s[1]);
+
+        int[] coins = new int[N];
+        for(int i = 0; i < N; i++){
+            coins[i] = Integer.parseInt(br.readLine());
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[computer + 1];
+        int total = K;
         int count = 0;
-        q.add(1);
-        visited[1] = true;
-
-        while (!q.isEmpty()) {
-            int node = q.poll();
-
-            for (int next : l.get(node)) {
-                if (!visited[next]) {
-                    q.add(next);
-                    visited[next] = true;
-                    count++;
-                }
+        for(int i = N-1; i>= 0; i--){
+            while(total >= coins[i]){
+                count += total / coins[i];
+                total = total % coins[i];
             }
         }
+
         bw.write(count + "\n");
         bw.flush();
         br.close();
