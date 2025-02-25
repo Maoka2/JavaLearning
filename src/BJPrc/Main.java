589031d0fa21638035d9c9788396afc78d976849
Maoka2 <a24358232@gmail.com>
2025-02-25 15:19:21 +0900

    백준 1764 듣보잡 https://www.acmicpc.net/problem/1764 HashSet

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,38 +8,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int M = Integer.parseInt(br.readLine());
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < M; i++) {
-            String[] s = br.readLine().split(" ");
-
-            if (s[0].equals("add")) {
-                set.add(Integer.parseInt(s[1]));
-            } else if (s[0].equals("check")) {
-                if (set.contains(Integer.parseInt(s[1]))) {
-                    bw.write(1 + "\n");
-                } else {
-                    bw.write(0 + "\n");
-                }
-            } else if (s[0].equals("remove")) {
-                set.remove(Integer.parseInt(s[1]));
-            } else if (s[0].equals("toggle")) {
-                if (set.contains(Integer.parseInt(s[1]))) {
-                    set.remove(Integer.parseInt(s[1]));
-                } else {
-                    set.add(Integer.parseInt(s[1]));
-                }
-            } else if (s[0].equals("all")) {
-                for (int j = 1; j <= 20; j++) {
-                    set.add(j);
-                }
-            } else if (s[0].equals("empty")) {
-                set.clear();
-            }
-
-
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        Set<String> set = new HashSet<>();
+        // 듣도 못한
+        for(int i = 0; i < N; i++){
+            set.add(br.readLine());
         }
 
+        String name;
+        int count = 0;
+        List<String> l = new ArrayList<>();
+        while((name = br.readLine()) != null){
+            if(set.contains(name)){
+                count++;
+                l.add(name);
+            }
+        }
+        bw.write(count + "\n");
+        Collections.sort(l);
+        for(String ss : l){
+            bw.write(ss + "\n");
+        }
         bw.flush();
         bw.close();
         br.close();
