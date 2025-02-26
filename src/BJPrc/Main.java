477a6e3918eccba35d919ec257fc7bcaeb5a3256
Maoka2 <a24358232@gmail.com>
2025-02-26 12:17:20 +0900

    dfs bfs 문제푸는도중저장

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,38 +9,62 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int N = Integer.parseInt(s[0]);
+        int M = Integer.parseInt(s[1]);
+        int V = Integer.parseInt(s[2]);
 
         List<List<Integer>> l = new ArrayList<>();
 
-        for(int i = 0; i <= N; i++){
+        for (int i = 0; i <= N; i++) {
             l.add(new ArrayList<>());
         }
-        for(int i = 0; i < N-1; i++){
-            String[] s = br.readLine().split(" ");
-            l.get(Integer.parseInt(s[0])).add(Integer.parseInt(s[1]));
-            l.get(Integer.parseInt(s[1])).add(Integer.parseInt(s[0]));
+        for (int i = 0; i < M; i++) {
+            String[] ss = br.readLine().split(" ");
+            l.get(Integer.parseInt(ss[0])).add(Integer.parseInt(ss[1]));
+            l.get(Integer.parseInt(ss[1])).add(Integer.parseInt(ss[0]));
+        }
+
+        for (int i = 0; i <= N; i++) {
+            Collections.sort(l.get(i));
         }
 
         Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[N+1];
-        int[] parent = new int[N+1];
-        q.add(1);
-        visited[1] = true;
+        Stack<Integer> stack = new Stack<>();
+        List<Integer> dfs = new ArrayList<>();
 
-        while(!q.isEmpty()){
-            int node = q.poll();
+        boolean visitedBFS[] = new boolean[N + 1];
+        boolean visitedDFS[] = new boolean[N + 1];
 
-            for(int next : l.get(node)){
-                if(!visited[next]){
-                    q.add(next);
-                    visited[next] = true;
-                    parent[next] = node;
+        q.add(V);
+        stack.push(V);
+
+        visitedDFS[V] = true;
+        visitedBFS[V] = true;
+
+        //DFS
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            dfs.add(node);
+            for (int next : l.get(node)) {
+                if (!visitedDFS[next]) {
+                    visitedDFS[next] = true;
+                    stack.push(next);
                 }
             }
         }
-        for(int i = 2; i <=N; i++){
-            bw.write(parent[i] + "\n");
+
+
+        //BFS
+        while (!q.isEmpty()) {
+            int node = q.poll();
+
+            for (int next : l.get(node)) {
+                if (!visitedBFS[next]) {
+                    q.add(next);
+                    visitedBFS[next] = true;
+                }
+            }
         }
 
         bw.flush();
