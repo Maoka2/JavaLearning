23c496f0e3169e7aa65ed5171b08565295476ae9
Maoka2 <a24358232@gmail.com>
2025-02-26 21:57:43 +0900

    백준 1021 회전하는 큐 https://www.acmicpc.net/problem/1021 인덱스에 따라서 회전방향 정하기

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -9,27 +9,54 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        String[] nums = br.readLine().split(" ");
-        Stack<Integer> stack = new Stack<>();
-
-        int[] temp = new int[N];
-        int[] result = new int[N];
+        String[] nm = br.readLine().split(" ");
+        int N = Integer.parseInt(nm[0]);
+        int M = Integer.parseInt(nm[1]);
+        Deque<Integer> d = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
-            temp[i] = Integer.parseInt(nums[i]);
-            result[i] = -1;
+            d.add(i + 1);
         }
-        for (int i = 0; i < N; i++) {
-            while (!stack.isEmpty() && temp[i] > temp[stack.peek()]) {
-                result[stack.pop()] = temp[i];
+
+        String[] s = br.readLine().split(" ");
+        int count = 0;
+
+        for (int i = 0; i < s.length; i++) {
+            // 왼쪽으로 돌지 오른쪽으로 돌지
+            int target = Integer.parseInt(s[i]);
+            boolean isClockwise = false;
+
+            if(d.peekFirst() == target){
+                d.pollFirst();
+                continue;
             }
-            stack.push(i);
-        }
 
-        for (int i = 0; i < N; i++) {
-            bw.write(result[i] + " ");
+            int index = 0;
+            for(int n : d){
+                if(n == target){
+                    break;
+                }
+                index++;
+            }
+
+            if (index > d.size() / 2) {
+                isClockwise = true;
+                // 반띵보다 뒤에 있으면 시계방향으로 도는게 이득 (맨 뒤를 맨 앞으로)
+                // 앞이면 반시계 (앞에꺼 맨 뒤로 보내기)
+            }
+            while(d.peekFirst() != target){
+                // 시계방향이면
+                if(isClockwise){
+                    d.addFirst(d.pollLast());
+                    count++;
+                } else{
+                    d.addLast(d.pollFirst());
+                    count++;
+                }
+            }
+            d.pollFirst();
         }
-        bw.write("\n");
+        bw.write(count + "\n");
+
 
         bw.flush();
         bw.close();
