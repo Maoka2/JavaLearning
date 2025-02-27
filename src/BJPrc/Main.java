47b43f8c354188772ef8f470d2d64cc5e522b740
Maoka2 <a24358232@gmail.com>
2025-02-27 15:11:31 +0900

    백준 15663 NM9 https://www.acmicpc.net/problem/15663 Set + 백트래킹?

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -6,6 +6,7 @@ import java.util.*;
 
 public class Main {
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static Set<String> printing = new HashSet<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,29 +14,43 @@ public class Main {
         String[] nm = br.readLine().split(" ");
         int N = Integer.parseInt(nm[0]);
         int M = Integer.parseInt(nm[1]);
-        int[] num = new int[M];
-        //boolean[] visited = new boolean[N + 1];
 
-        backTracking(num,N,M,0);
+        int[] numArray = new int[N + 1];
+        String[] numbers = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            numArray[i] = Integer.parseInt(numbers[i - 1]);
+        }
+        Arrays.sort(numArray);
+        boolean[] visitied = new boolean[N + 1];
+        int[] result = new int[M];
+        backTracking(result, numArray, N, M, 0,visitied);
 
 
         bw.flush();
         bw.close();
         br.close();
     }
-    static void backTracking(int[] arr, int N, int M, int depth) throws IOException{
-        if(depth == M){
-            for(int i = 0; i < M; i++){
-                bw.write(arr[i] + " ");
+
+    static void backTracking(int[] result, int[] arr, int N, int M, int depth, boolean[] visited) throws IOException {
+        if (depth == M) {
+            String s = "";
+            for (int i = 0; i < M; i++) {
+                s += result[i] + " ";
             }
-            bw.write("\n");
+            if(!printing.contains(s)){
+                printing.add(s);
+                bw.write(s + "\n");
+            }
+
             return;
         }
 
-        for(int i = 1; i <= N; i++){
-            if(depth == 0 || arr[depth -1] <= i){
-                arr[depth] = i;
-                backTracking(arr,N,M,depth + 1);
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                result[depth] = arr[i];
+                backTracking(result, arr, N, M, depth + 1, visited);
+                visited[i] = false;
             }
         }
     }
