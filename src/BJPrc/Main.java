2d3240f680200ab2177cc8391c64b083396a8397
Maoka2 <a24358232@gmail.com>
2025-02-27 14:12:56 +0900

    백준 15652 NM4 https://www.acmicpc.net/problem/15652 중복허용하면서 비내림차순 백트래킹

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -14,34 +14,33 @@ public class Main {
         int N = Integer.parseInt(nm[0]);
         int M = Integer.parseInt(nm[1]);
         int[] num = new int[M];
-        boolean[] visited = new boolean[N+1];
+        //boolean[] visited = new boolean[N + 1];
 
-        backTracking(num,visited,N,M,0);
+        backTracking(num,N,M,0);
+
 
         bw.flush();
         bw.close();
         br.close();
     }
-
-    static void backTracking(int[] arr, boolean[] visited, int N, int M, int depth) throws IOException {
-        if (depth == M) {
-            for (int i = 0; i < M; i++) {
+    static void backTracking(int[] arr, int N, int M, int depth) throws IOException{
+        if(depth == M){
+            for(int i = 0; i < M; i++){
                 bw.write(arr[i] + " ");
             }
             bw.write("\n");
             return;
         }
 
-        for(int i = 1; i <= N; i++){ // 오름차순이니까 이미 되있는거보다는 지금 탐색?하는게 크게끔
-           // if((depth == 0 || i >= arr[depth-1])){
-                //visited[i] = true;
+        for(int i = 1; i <= N; i++){
+            if(depth == 0 || arr[depth -1] <= i){
                 arr[depth] = i;
-                backTracking(arr, visited, N, M, depth+1);
-                //visited[i] = false;
-
+                backTracking(arr,N,M,depth + 1);
             }
         }
     }
+
+}
 
 
 
