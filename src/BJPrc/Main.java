14c2a86bec49b2b28b4db3bf087666fb39a2fab0
Maoka2 <a24358232@gmail.com>
2025-02-27 14:06:52 +0900

    백준 15651 NM3 https://www.acmicpc.net/problem/15651 중복허용 백트래킹

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -13,11 +13,8 @@ public class Main {
         String[] nm = br.readLine().split(" ");
         int N = Integer.parseInt(nm[0]);
         int M = Integer.parseInt(nm[1]);
-        int[] num = new int[N+1];
+        int[] num = new int[M];
         boolean[] visited = new boolean[N+1];
-        for(int i = 1; i <= N; i++){
-            num[i] = i;
-        }
 
         backTracking(num,visited,N,M,0);
 
@@ -32,17 +29,19 @@ public class Main {
                 bw.write(arr[i] + " ");
             }
             bw.write("\n");
+            return;
         }
 
         for(int i = 1; i <= N; i++){ // 오름차순이니까 이미 되있는거보다는 지금 탐색?하는게 크게끔
-            if(!visited[i] && (depth == 0 || i > arr[depth-1])){
-                visited[i] = true;
+           // if((depth == 0 || i >= arr[depth-1])){
+                //visited[i] = true;
                 arr[depth] = i;
                 backTracking(arr, visited, N, M, depth+1);
-                visited[i] = false;
+                //visited[i] = false;
+
             }
         }
     }
 
 
-}
+
