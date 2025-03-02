7d2a160c7f7e81de7b6dac5a77bab1e0afe38d6d
Maoka2 <a24358232@gmail.com>
2025-03-02 17:11:26 +0900

    백준 1918 후위 표기식 https://www.acmicpc.net/problem/1918 스택

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,37 +8,39 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] np = br.readLine().split(" ");
-        int N = Integer.parseInt(np[0]);
-        int P = Integer.parseInt(np[1]);
+        String s = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    sb.append(stack.pop());
+                }
+                stack.pop(); //여는거 제거
+            } else if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                sb.append(s.charAt(i));
+            } else {
+                while (!stack.isEmpty() && ((stack.peek() == '*' || stack.peek() == '/') ||
+                        (s.charAt(i) == '+' || s.charAt(i) == '-') && (stack.peek() == '+' || stack.peek() == '-'))) {
+                    sb.append(stack.pop());
 
-        // 현재 프렛? 보다 크면 그냥 누르고있고 작아지면 떼버리기?
-        ArrayList<Stack<Integer>> l = new ArrayList<>();
 
-        int count = 0;
-
-        for(int i = 0; i < 6; i++){
-            l.add(new Stack<>());
-        }
-
-        for(int i = 0; i < N; i++){
-            String[] s = br.readLine().split(" ");
-            int line = Integer.parseInt(s[0]);
-            int vmfpt = Integer.parseInt(s[1]);
-
-
-            while(!l.get(line-1).isEmpty() && l.get(line-1).peek() > vmfpt){
-                l.get(line-1).pop();
-                count++;
+                }
+                stack.push(s.charAt(i));
             }
 
-            if(l.get(line-1).isEmpty() || l.get(line-1).peek() != vmfpt){
-                l.get(line-1).push(vmfpt);
-                count++;
-            }
+
+
+
+        }
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
         }
 
-        bw.write(count + "\n");
+        bw.write(sb + "\n");
+
         bw.flush();
         br.close();
         bw.close();
