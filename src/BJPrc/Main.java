4a869b5ed070a5c5d1a9b430842ca208750220f0
Maoka2 <a24358232@gmail.com>
2025-03-02 14:35:54 +0900

    백준 3078 좋은 친구 https://www.acmicpc.net/problem/3078 큐, 인덱스로 배열관리, long

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,85 +4,49 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] nmv = br.readLine().split(" ");
-        int N = Integer.parseInt(nmv[0]);
-        int M = Integer.parseInt(nmv[1]);
-        int V = Integer.parseInt(nmv[2]);
 
-        List<List<Integer>> l = new ArrayList<>();
-        for (int i = 0; i <= N; i++) {
-            l.add(new ArrayList<>());
+        // 친구 -> 자신과 반 등수 차이가 K를 넘으면 친구가 아니라는거
+        // 좋은 친구 -> 이름의 길이가 같아야, 등수 차이가 K이하이면서
+
+        String[] nk = br.readLine().split(" ");
+        int N = Integer.parseInt(nk[0]);
+        int K = Integer.parseInt(nk[1]);
+
+        long count = 0;
+
+        int[] nameLength = new int[N];
+
+        for(int i = 0; i < N; i++){
+            nameLength[i] = br.readLine().length();
+        }
+        int[] lengths = new int[21];
+
+        Deque<Integer> d = new ArrayDeque<>();
+
+        for(int i = 0; i < N; i++){
+            int length = nameLength[i];
+            count += lengths[length];
+
+            d.addLast(length);
+            lengths[length]++;
+
+            if(d.size() > K){
+                int remove = d.pollFirst();
+                lengths[remove]--;
+            }
         }
 
-        for (int i = 0; i < M; i++) {
-            String[] s = br.readLine().split(" ");
-            int a = Integer.parseInt(s[0]);
-            int b = Integer.parseInt(s[1]);
 
-            l.get(a).add(b);
-            l.get(b).add(a);
 
-        }
-        for(int i = 0; i <= N; i++){
-            Collections.sort(l.get(i));
-        }
 
-        //DFS
-        boolean[] visited1 = new boolean[N+1];
-        dfs(V,l,visited1);
-        bw.write("\n");
-        //BFS
-        boolean[] visited2 = new boolean[N+1];
-        bfs(V,l,visited2);
-        bw.write("\n");
-
+        bw.write(count + "\n");
         bw.flush();
-        bw.close();
         br.close();
-    }
-    static void dfs(int node, List<List<Integer>> list, boolean[] visited) throws IOException{
-        Stack<Integer> stack = new Stack<>();
-        stack.push(node);
+        bw.close();
 
-        while(!stack.isEmpty()){
-            int current = stack.pop();
-
-            if(!visited[current]){
-                visited[current] = true;
-                bw.write(current + " ");
-
-                List<Integer> adjacent = list.get(current);
-
-                for(int i = adjacent.size()-1; i >=0; i--){
-                    if(!visited[adjacent.get(i)]){
-                        stack.push(adjacent.get(i));
-                    }
-                }
-            }
-        }
-    }
-
-    static void bfs(int node, List<List<Integer>> list, boolean[] visited) throws IOException{
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(node);
-        visited[node] = true;
-
-        while(!q.isEmpty()){
-            int current = q.poll();
-            bw.write(current + " ");
-
-            for(int next : list.get(current)){
-                if(!visited[next]){
-                    visited[next] = true;
-                    q.add(next);
-                }
-            }
-        }
     }
 }
