9ab6c9a89747bcf168f8740324307977b15f6194
Maoka2 <a24358232@gmail.com>
2025-03-02 22:18:17 +0900

    백준 2840 행운의 룰렛 https://www.acmicpc.net/problem/2840 시계반시계

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,44 +8,55 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
-        // 백스페이스 -
+        String[] nk = br.readLine().split(" ");
+        int N = Integer.parseInt(nk[0]); // 바퀴칸
+        int K = Integer.parseInt(nk[1]); // 몇번돌릴지
 
-
-        for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            LinkedList<Character> ll = new LinkedList<>();
-            LinkedList<Character> ans = new LinkedList<>();
-
-            for (char c : s.toCharArray()) {
-                ll.add(c);
-            }
-            int idx = 0;
-            while (!ll.isEmpty()) {
-                char current = ll.pollFirst();
-                if (current == '<') {
-                    if (idx > 0) {
-                        idx--;
-                    }
-                } else if (current == '>') {
-                    if (idx < ans.size()) {
-                        idx++;
-                    }
-                } else if (current == '-') {
-                    if (idx > 0) {
-                        ans.remove(idx - 1);
-                        idx--;
-                    }
-                } else {
-                    ans.add(idx, current);
-                    idx++;
-                }
-            }
-            for(char c : ans){
-                bw.write(c);
-            }
-            bw.write("\n");
+        char[] fnffpt = new char[N];
+        for(int i = 0; i < N; i++){
+            fnffpt[i] = '?';
         }
+
+        boolean[] alphabet = new boolean[26];
+        int current = 0;
+        for(int i = 0; i < K; i++){
+            String[] s = br.readLine().split(" ");
+            int next = Integer.parseInt(s[0]);
+            current = current - next % N;
+            if(current < 0){
+                current += N;
+            }
+
+            if(fnffpt[current] != '?' && fnffpt[current] != s[1].charAt(0)){
+                bw.write("!\n");
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            } // 이미 뭔가 있는경우
+
+            // 이미 쓴거라면
+            if(fnffpt[current] == '?' && alphabet[s[1].charAt(0) - 'A']){
+                bw.write("!\n");
+                bw.flush();
+                bw.close();
+                br.close();
+                return;
+            }
+            fnffpt[current] = s[1].charAt(0);
+            alphabet[s[1].charAt(0) - 'A'] = true;
+
+        }
+
+
+
+        for(int i = 0; i < N; i++){
+            bw.write(fnffpt[(current + i) % N]);
+        }
+        bw.write("\n");
+
+
+
 
 
         bw.flush();
