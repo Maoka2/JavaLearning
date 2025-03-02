26967d8bc52343abb748da157b02311c6e41c09f
Maoka2 <a24358232@gmail.com>
2025-03-02 16:01:46 +0900

    백준 3986 창고 다각형 https://www.acmicpc.net/problem/3986 스택

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,53 +8,29 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // ㅇ목한게 없다 -> 오름차순 -> 내림차순
-
         int N = Integer.parseInt(br.readLine());
-        int init = Integer.MAX_VALUE;
-        int end = Integer.MIN_VALUE;
-        int max = -1;
-        int maxIdx = 0;
+        int count = 0;
 
-        int[] arr = new int[1001];
-        for (int i = 0; i < N; i++) {
-            String[] s = br.readLine().split(" ");
-            int L = Integer.parseInt(s[0]);
-            int H = Integer.parseInt(s[1]);
+        // 좋은 단어? 뭔소리임
 
-            arr[L] = H;
-            init = Math.min(init, L);
-            end = Math.max(end, L);
-            max = Math.max(max,H);
-            if(max == H){
-                maxIdx = L;
+        for(int i = 0; i < N; i++){
+            Stack<Character> stack = new Stack<>();
+
+            String s = br.readLine();
+
+            for(int j = 0; j < s.length(); j++){
+                if(!stack.isEmpty() && stack.peek() == s.charAt(j)){
+                    stack.pop();
+                }else{
+                    stack.push(s.charAt(j));
+                }
+            }
+
+            if(stack.isEmpty()){
+                count++;
             }
         }
-
-        int height = arr[init];
-        int s = 0;
-
-        for (int i = init + 1; i <= maxIdx; i++) {
-            if (arr[i] >= height) {
-                s += (i - init) * height;
-                height = arr[i];
-                init = i;
-            }
-        }
-
-        height = arr[end];
-
-        for (int i = end - 1; i >= maxIdx; i--) {
-            if (arr[i] >= height) {
-                s += height * (end - i);
-                height = arr[i];
-                end = i;
-            }
-        }
-
-        s += max;
-        bw.write(s + "\n");
-
+        bw.write(count + "\n");
         bw.flush();
         br.close();
         bw.close();
