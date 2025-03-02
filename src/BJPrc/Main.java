3e407764a3a352f2b8f907992114379ed2d341a1
Maoka2 <a24358232@gmail.com>
2025-03-02 15:46:05 +0900

    백준 2304 창고 다각형 https://www.acmicpc.net/problem/2304 스택

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -8,48 +8,55 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        // ㅇ목한게 없다 -> 오름차순 -> 내림차순
+
         int N = Integer.parseInt(br.readLine());
-        String s = br.readLine();
-        double result = 0;
-        double[] numbers = new double[N];
+        int init = Integer.MAX_VALUE;
+        int end = Integer.MIN_VALUE;
+        int max = -1;
+        int maxIdx = 0;
 
-        for(int i = 0; i < N; i++){
-            numbers[i] = Double.parseDouble(br.readLine());
-        }
+        int[] arr = new int[1001];
+        for (int i = 0; i < N; i++) {
+            String[] s = br.readLine().split(" ");
+            int L = Integer.parseInt(s[0]);
+            int H = Integer.parseInt(s[1]);
 
-        Stack<Double> stack = new Stack<>();
-
-        for(int i = 0; i < s.length(); i++){
-            if( s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
-                stack.push(numbers[s.charAt(i)-'A']);
-            }else{ // 연산자
-                double a = stack.pop();
-                double b = stack.pop();
-
-                switch(s.charAt(i)){
-                    case '+':
-                        stack.push(b+a);
-                        break;
-
-                    case '-':
-                        stack.push(b-a);
-                        break;
-
-                    case '*':
-                        stack.push(b*a);
-                        break;
-
-                    case '/':
-                        stack.push(b/a);
-                        break;
-                }
+            arr[L] = H;
+            init = Math.min(init, L);
+            end = Math.max(end, L);
+            max = Math.max(max,H);
+            if(max == H){
+                maxIdx = L;
             }
         }
-        bw.write(String.format("%.2f",stack.pop()) + "\n");
+
+        int height = arr[init];
+        int s = 0;
+
+        for (int i = init + 1; i <= maxIdx; i++) {
+            if (arr[i] >= height) {
+                s += (i - init) * height;
+                height = arr[i];
+                init = i;
+            }
+        }
+
+        height = arr[end];
+
+        for (int i = end - 1; i >= maxIdx; i--) {
+            if (arr[i] >= height) {
+                s += height * (end - i);
+                height = arr[i];
+                end = i;
+            }
+        }
+
+        s += max;
+        bw.write(s + "\n");
 
         bw.flush();
         br.close();
         bw.close();
-
     }
 }
