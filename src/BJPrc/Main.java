a34100e039ff76085532958b0376969fbe885571
Maoka2 <a24358232@gmail.com>
2025-03-03 15:15:58 +0900

    백준 5014 스타트링크 https://www.acmicpc.net/problem/5014 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,87 +4,63 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N,M,K;
-    static int[][] squares;
-    static boolean[][] visited;
+    static int F, S, G, U, D;
+    static int[] floors;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //F 총 층 S 현재층  G 스타트링크
 
-        String[] mnk = br.readLine().split(" ");
-        M = Integer.parseInt(mnk[0]);
-        N = Integer.parseInt(mnk[1]);
-        K = Integer.parseInt(mnk[2]);
-        squares = new int[M][N];
-        visited = new boolean[M][N];
-        for(int i = 0; i < K; i++){
-            String[] s = br.readLine().split(" ");
-            int startX = Integer.parseInt(s[0]);
-            int startY = Integer.parseInt(s[1]);
-            int endX = Integer.parseInt(s[2]);
-            int endY = Integer.parseInt(s[3]);
+        String[] s = br.readLine().split(" ");
+        F = Integer.parseInt(s[0]);
+        S = Integer.parseInt(s[1]);
+        G = Integer.parseInt(s[2]);
+        U = Integer.parseInt(s[3]);
+        D = Integer.parseInt(s[4]);
 
-            for(int j = startX; j < endX; j++){
-                for(int k = startY; k < endY; k++){
-                    squares[k][j] = 1;
-                }
-            }
+        floors = new int[F + 1];
+        visited = new boolean[F + 1];
+
+        int result = bfs(S,G);
+
+        if(result == -1){
+            bw.write("use the stairs\n");
+        } else{
+            bw.write(result + "\n");
         }
-
-        int count = 0;
-
-        List<Integer> l = new ArrayList<>();
-
-        for(int i = 0; i < M; i++){
-            for(int j = 0; j < N; j++){
-                if(squares[i][j] == 0 && !visited[i][j]){
-                    l.add(bfs(i,j));
-                    count++;
-                }
-            }
-        }
-        Collections.sort(l);
-
-        bw.write(count + "\n");
-        for(int i = 0; i < l.size(); i++){
-            bw.write(l.get(i) + " ");
-        }
-        bw.write("\n");
 
         bw.flush();
         br.close();
         bw.close();
     }
 
-    static int bfs(int x, int y){
+    static int bfs(int S, int G) {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
-        visited[x][y] = true;
-        int size = 1;
+        int count = 0;
+        q.add(new int[]{S, count});
+        visited[S] = true;
 
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int[] current = q.poll();
-            int cx = current[0];
-            int cy = current[1];
+            int floor = current[0];
+            count = current[1];
 
-            for(int i = 0; i < 4; i++){
-                int mx = cx + dx[i];
-                int my = cy + dy[i];
+            if (floor == G) {
+                return count;
+            }
+            int[] move = {floor + U, floor - D};
+            for (int i = 0; i < move.length; i++) {
+                int next = move[i];
 
-                if(mx >= 0 && my >= 0 && mx < M && my < N){
-                    if(squares[mx][my] == 0 && !visited[mx][my]){
-                        visited[mx][my] = true;
-                        q.add(new int[]{mx,my});
-                        size++;
-                    }
+                if (next >= 1 && next <= F && !visited[next]) {
+                    visited[next] = true;
+                    q.add(new int[]{next, count + 1});
                 }
             }
-
         }
-        return size;
+        return -1;
     }
 }
 
