53f5c30070826ab35e1362fd1d78c4fb077becf3
Maoka2 <a24358232@gmail.com>
2025-03-03 14:58:22 +0900

    백준 2583 영역구하기 https://www.acmicpc.net/problem/2583 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,54 +4,87 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, K;
-    static boolean visited[];
-
+    static int N,M,K;
+    static int[][] squares;
+    static boolean[][] visited;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] nk = br.readLine().split(" ");
-        N = Integer.parseInt(nk[0]);
-        K = Integer.parseInt(nk[1]);
-        visited = new boolean[100001];
+        String[] mnk = br.readLine().split(" ");
+        M = Integer.parseInt(mnk[0]);
+        N = Integer.parseInt(mnk[1]);
+        K = Integer.parseInt(mnk[2]);
+        squares = new int[M][N];
+        visited = new boolean[M][N];
+        for(int i = 0; i < K; i++){
+            String[] s = br.readLine().split(" ");
+            int startX = Integer.parseInt(s[0]);
+            int startY = Integer.parseInt(s[1]);
+            int endX = Integer.parseInt(s[2]);
+            int endY = Integer.parseInt(s[3]);
 
-        bw.write(bfs(N, K) + "\n");
+            for(int j = startX; j < endX; j++){
+                for(int k = startY; k < endY; k++){
+                    squares[k][j] = 1;
+                }
+            }
+        }
+
+        int count = 0;
+
+        List<Integer> l = new ArrayList<>();
+
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(squares[i][j] == 0 && !visited[i][j]){
+                    l.add(bfs(i,j));
+                    count++;
+                }
+            }
+        }
+        Collections.sort(l);
+
+        bw.write(count + "\n");
+        for(int i = 0; i < l.size(); i++){
+            bw.write(l.get(i) + " ");
+        }
+        bw.write("\n");
+
         bw.flush();
         br.close();
         bw.close();
     }
 
-    static int bfs(int N, int K) {
+    static int bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
-        int count = 0;
-        q.add(new int[]{N, count});
-        visited[N] = true;
+        q.add(new int[]{x,y});
+        visited[x][y] = true;
+        int size = 1;
 
-        if (N == K) {
-            return 0;
-        }
-        while (!q.isEmpty()) {
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
+
+        while(!q.isEmpty()){
             int[] current = q.poll();
-            int x = current[0];
-            count = current[1];
+            int cx = current[0];
+            int cy = current[1];
 
-            if (x == K) {
-                return count;
-            }
+            for(int i = 0; i < 4; i++){
+                int mx = cx + dx[i];
+                int my = cy + dy[i];
 
-            int[] dx = {x * 2, x + 1, x - 1};
-
-            for (int i = 0; i < dx.length; i++) {
-                int next = dx[i];
-                if (next >= 0 && next < 100001 && !visited[next]) {
-                    visited[next] = true;
-                    q.add(new int[]{next, count + 1});
+                if(mx >= 0 && my >= 0 && mx < M && my < N){
+                    if(squares[mx][my] == 0 && !visited[mx][my]){
+                        visited[mx][my] = true;
+                        q.add(new int[]{mx,my});
+                        size++;
+                    }
                 }
             }
-        }
 
-        return -1;
+        }
+        return size;
     }
 }
 
