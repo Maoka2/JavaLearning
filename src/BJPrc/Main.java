e3e7a5bf68e95310ea938d19bb19c498ba1af285
Maoka2 <a24358232@gmail.com>
2025-03-03 14:00:14 +0900

    백준 7562 나이트의이동 https://www.acmicpc.net/problem/7562 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,77 +4,71 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N;
-    static int map[][];
+    static int I;
+    static int[][] chess;
+    static int startX,startY, endX, endY;
     static boolean visited[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
+        int T = Integer.parseInt(br.readLine());
 
-        map = new int[N][N];
-        visited = new boolean[N][N];
+        for(int tc = 0; tc < T; tc++){
+            I = Integer.parseInt(br.readLine());
+            chess = new int[I][I];
+            visited = new boolean[I][I];
+            String[] current = br.readLine().split(" ");
+            startX = Integer.parseInt(current[0]);
+            startY = Integer.parseInt(current[1]);
 
-        for(int i = 0; i < N; i++){
-            String s = br.readLine();
-            for(int j = 0; j < N; j++){
-                map[i][j] = s.charAt(j) - '0';
-            }
+            String[] end = br.readLine().split(" ");
+            endX = Integer.parseInt(end[0]);
+            endY = Integer.parseInt(end[1]);
+
+            bw.write(bfs(startX,startY,endX,endY) + "\n");
         }
-
-        int total = 0;
-        List<Integer> houseNum = new ArrayList<>();
-        for(int i = 0; i < N; i++){
-            for(int j = 0; j < N; j++){
-                if(map[i][j] == 1 && !visited[i][j]){
-                    houseNum.add(bfs(i,j));
-                    total++;
-                }
-            }
-        }
-
-        Collections.sort(houseNum);
-
-        bw.write(total + "\n");
-        for(int i = 0; i < houseNum.size(); i++){
-            bw.write(houseNum.get(i) + "\n");
-        }
-
-
 
         bw.flush();
         br.close();
         bw.close();
     }
-    static int bfs(int x, int y){
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{x,y});
-        visited[x][y] = true;
-        int count = 1;
+    static int bfs(int startX, int startY, int endX, int endY){
+        if(startX == endX && startY == endY){
+            return 0;
+        }
 
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
+        int[] dx = {-1,-2,-2,-1,1,2,2,1}; // 그림 그 11시 방향부터 시계방향으로
+        int[] dy = {-2,-1,1,2,2,1,-1,-2}; // 그림 그 11시 방향부터 시계방향으로
+        int[][] count = new int[I][I];
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{startX,startY});
+        count[startX][startY] = 0;
+        visited[startX][startY] = true;
 
         while(!q.isEmpty()){
             int[] current = q.poll();
             int cx = current[0];
             int cy = current[1];
 
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < dx.length; i++){
                 int mx = cx + dx[i];
                 int my = cy + dy[i];
 
-                if(mx >= 0 && my >= 0 && mx < N && my < N){
-                    if(map[mx][my] == 1 && !visited[mx][my]){
+                if(mx >= 0 && my >= 0 && mx < I && my < I){
+                    if(!visited[mx][my]){
                         visited[mx][my] = true;
-                        count++;
                         q.add(new int[]{mx,my});
+                        count[mx][my] = count[cx][cy] + 1;
+
+                        if(mx == endX && my == endY){
+                            return count[mx][my];
+                        }
                     }
                 }
             }
         }
-        return count;
+        return -1;
     }
 }
 
