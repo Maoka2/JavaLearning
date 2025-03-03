4cbbb5edf911453482afb2abf52ae0966e797dc6
Maoka2 <a24358232@gmail.com>
2025-03-03 14:39:35 +0900

    백준 1697 숨바꼭질 https://www.acmicpc.net/problem/1697 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,70 +4,53 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int I;
-    static int[][] chess;
-    static int startX,startY, endX, endY;
-    static boolean visited[][];
+    static int N, K;
+    static boolean visited[];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
+        String[] nk = br.readLine().split(" ");
+        N = Integer.parseInt(nk[0]);
+        K = Integer.parseInt(nk[1]);
+        visited = new boolean[100001];
 
-        for(int tc = 0; tc < T; tc++){
-            I = Integer.parseInt(br.readLine());
-            chess = new int[I][I];
-            visited = new boolean[I][I];
-            String[] current = br.readLine().split(" ");
-            startX = Integer.parseInt(current[0]);
-            startY = Integer.parseInt(current[1]);
-
-            String[] end = br.readLine().split(" ");
-            endX = Integer.parseInt(end[0]);
-            endY = Integer.parseInt(end[1]);
-
-            bw.write(bfs(startX,startY,endX,endY) + "\n");
-        }
-
+        bw.write(bfs(N, K) + "\n");
         bw.flush();
         br.close();
         bw.close();
     }
-    static int bfs(int startX, int startY, int endX, int endY){
-        if(startX == endX && startY == endY){
+
+    static int bfs(int N, int K) {
+        Queue<int[]> q = new LinkedList<>();
+        int count = 0;
+        q.add(new int[]{N, count});
+        visited[N] = true;
+
+        if (N == K) {
             return 0;
         }
-
-        int[] dx = {-1,-2,-2,-1,1,2,2,1}; // 그림 그 11시 방향부터 시계방향으로
-        int[] dy = {-2,-1,1,2,2,1,-1,-2}; // 그림 그 11시 방향부터 시계방향으로
-        int[][] count = new int[I][I];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startX,startY});
-        count[startX][startY] = 0;
-        visited[startX][startY] = true;
-
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             int[] current = q.poll();
-            int cx = current[0];
-            int cy = current[1];
+            int x = current[0];
+            count = current[1];
 
-            for(int i = 0; i < dx.length; i++){
-                int mx = cx + dx[i];
-                int my = cy + dy[i];
+            if (x == K) {
+                return count;
+            }
 
-                if(mx >= 0 && my >= 0 && mx < I && my < I){
-                    if(!visited[mx][my]){
-                        visited[mx][my] = true;
-                        q.add(new int[]{mx,my});
-                        count[mx][my] = count[cx][cy] + 1;
+            int[] dx = {x * 2, x + 1, x - 1};
 
-                        if(mx == endX && my == endY){
-                            return count[mx][my];
-                        }
-                    }
+            for (int i = 0; i < dx.length; i++) {
+                int next = dx[i];
+                if (next >= 0 && next < 100001 && !visited[next]) {
+                    visited[next] = true;
+                    q.add(new int[]{next, count + 1});
                 }
             }
         }
+
         return -1;
     }
 }
