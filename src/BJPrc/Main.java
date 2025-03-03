e3b2caee95431fcade954555d0af43b55ce021e3
Maoka2 <a24358232@gmail.com>
2025-03-03 13:26:25 +0900

    백준 2667 단지번호붙이기 https://www.acmicpc.net/problem/2667 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,45 +4,50 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, K;
-    static int[][] xhdfh;
-    static boolean[][] visited;
+    static int N;
+    static int map[][];
+    static boolean visited[][];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] nmk = br.readLine().split(" ");
-        N = Integer.parseInt(nmk[0]);
-        M = Integer.parseInt(nmk[1]);
-        K = Integer.parseInt(nmk[2]);
+        N = Integer.parseInt(br.readLine());
 
-        xhdfh = new int[N][M];
-        visited = new boolean[N][M];
-        for (int i = 0; i < K; i++) {
-            String[] rc = br.readLine().split(" ");
-            int r = Integer.parseInt(rc[0]) - 1;
-            int c = Integer.parseInt(rc[1]) - 1;
-
-            xhdfh[r][c] = 1;
-        }
-
-        int max = 0;
+        map = new int[N][N];
+        visited = new boolean[N][N];
 
         for(int i = 0; i < N; i++){
-            for(int j = 0; j < M; j++){
-                if(xhdfh[i][j] == 1 && !visited[i][j]){
-                    max = Math.max(max,bfs(i,j));
+            String s = br.readLine();
+            for(int j = 0; j < N; j++){
+                map[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        int total = 0;
+        List<Integer> houseNum = new ArrayList<>();
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < N; j++){
+                if(map[i][j] == 1 && !visited[i][j]){
+                    houseNum.add(bfs(i,j));
+                    total++;
                 }
             }
         }
 
-        bw.write(max + "\n");
+        Collections.sort(houseNum);
+
+        bw.write(total + "\n");
+        for(int i = 0; i < houseNum.size(); i++){
+            bw.write(houseNum.get(i) + "\n");
+        }
+
+
+
         bw.flush();
         br.close();
         bw.close();
     }
-
-    static int bfs(int x, int y) {
+    static int bfs(int x, int y){
         Queue<int[]> q = new LinkedList<>();
         q.add(new int[]{x,y});
         visited[x][y] = true;
@@ -60,10 +65,10 @@ public class Main {
                 int mx = cx + dx[i];
                 int my = cy + dy[i];
 
-                if(mx >= 0 && my >= 0 && my < M && mx < N){
-                    if(xhdfh[mx][my] == 1 && !visited[mx][my]){
-                        count++;
+                if(mx >= 0 && my >= 0 && mx < N && my < N){
+                    if(map[mx][my] == 1 && !visited[mx][my]){
                         visited[mx][my] = true;
+                        count++;
                         q.add(new int[]{mx,my});
                     }
                 }
