9f3be78b3593512c4aefb70d4b755c605d223278
Maoka2 <a24358232@gmail.com>
2025-03-03 12:52:17 +0900

    백준 1012 유기농배추 https://www.acmicpc.net/problem/1012 BFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,63 +4,77 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N,M;
-    static boolean[] visited;
-    static ArrayList<ArrayList<Integer>> l = new ArrayList<>();
+    static int N,M,K;
+    static boolean[][] visited;
+    static int[][] qocn;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        String[] nm = br.readLine().split(" ");
+        int T = Integer.parseInt(br.readLine());
 
-        N = Integer.parseInt(nm[0]);
-        M = Integer.parseInt(nm[1]);
+        for(int tc = 0; tc < T; tc++) {
 
-        for(int i = 0; i <= N; i++){
-            l.add(new ArrayList<>());
-        }
 
-        for(int i = 0; i < M; i++){
-            String[] uv = br.readLine().split(" ");
-            int u = Integer.parseInt(uv[0]);
-            int v = Integer.parseInt(uv[1]);
+            String[] mnk = br.readLine().split(" ");
 
-            l.get(u).add(v);
-            l.get(v).add(u);
-        }
+            M = Integer.parseInt(mnk[0]);
+            N = Integer.parseInt(mnk[1]);
+            K = Integer.parseInt(mnk[2]);
 
-        visited = new boolean[N+1];
+            qocn = new int[N][M];
 
-        int count = 0;
-        for(int i = 1; i <= N; i++){
-            if(!visited[i]){
-                bfs(i);
-                count++;
+            for (int i = 0; i < K; i++) {
+                String[] s = br.readLine().split(" ");
+                int x = Integer.parseInt(s[0]);
+                int y = Integer.parseInt(s[1]);
+
+                qocn[y][x] = 1;
             }
+
+            visited = new boolean[N][M];
+            int count = 0;
+            for(int i = 0; i < N; i++){
+                for(int j = 0; j < M; j++){
+                    if(qocn[i][j] == 1  && ! visited[i][j]){
+                        bfs(i,j);
+                        count++;
+                    }
+                }
+            }
+            bw.write(count + "\n");
         }
-
-
-        bw.write(count + "\n");
-
         bw.flush();
         br.close();
         bw.close();
     }
-
-    static void bfs(int start){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        visited[start] = true;
+    static void bfs(int y, int x){
+        Queue<int[]> q = new LinkedList<>();
+        int[] dy = {-1,1,0,0};
+        int[] dx = {0,0,-1,1};
+        q.add(new int[]{y,x});
+        visited[y][x] = true;
 
         while(!q.isEmpty()){
-            int node = q.poll();
-            for(int next : l.get(node)){
-                if(!visited[next]){
-                    visited[next] = true;
-                    q.add(next);
+            int[] node = q.poll();
+            int cy = node[0];
+            int cx = node[1];
+
+            for(int i = 0; i < 4; i++){
+                int mx = cx + dx[i];
+                int my = cy + dy[i];
+
+                if(mx >= 0 && my >= 0 && mx < M && my < N){
+                    if(qocn[my][mx] == 1 && ! visited[my][mx]){
+                           visited[my][mx] = true;
+                        q.add(new int[]{my,mx});
+                    }
                 }
             }
+
+
         }
+
     }
 }
 
