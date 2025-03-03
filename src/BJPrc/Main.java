17ba59fb07e071c97fb0c169ecba4aac881f18e4
Maoka2 <a24358232@gmail.com>
2025-03-03 16:23:14 +0900

    백준 24480 DFS2 https://www.acmicpc.net/problem/24480 DFS

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -4,11 +4,11 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int N, M, R;
-    static ArrayList<ArrayList<Integer>> l = new ArrayList<>();
+    static int N,M,R;
     static boolean[] visited;
-    static int[] nodes;
+    static int[] order;
     static int count = 0;
+    static ArrayList<ArrayList<Integer>> l = new ArrayList<>();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -17,46 +17,45 @@ public class Main {
         N = Integer.parseInt(nmr[0]);
         M = Integer.parseInt(nmr[1]);
         R = Integer.parseInt(nmr[2]);
-
+        order = new int[N+1];
+        visited = new boolean[N+1];
         for(int i = 0; i <= N; i++){
             l.add(new ArrayList<>());
         }
-        nodes = new int[N+1];
-        visited = new boolean[N+1];
 
         for(int i = 0; i < M; i++){
-            String[] s = br.readLine().split(" ");
-            int u = Integer.parseInt(s[0]);
-            int v = Integer.parseInt(s[1]);
+            String[] uv = br.readLine().split(" ");
+            int u = Integer.parseInt(uv[0]);
+            int v = Integer.parseInt(uv[1]);
 
             l.get(u).add(v);
             l.get(v).add(u);
-
         }
 
         for(int i = 1; i <= N; i++){
-            Collections.sort(l.get(i));
+            l.get(i).sort(Collections.reverseOrder());
         }
+
 
         dfs(R);
 
-        for(int i = 1; i < nodes.length; i++){
-            bw.write(nodes[i] + "\n");
+        for(int i = 1; i <= N; i++){
+            bw.write(order[i] + "\n");
         }
-
         bw.flush();
         br.close();
         bw.close();
     }
-    static void dfs(int node){
-        visited[node] = true;
-        nodes[node] = ++count;
+    static void dfs(int R){
+        visited[R] = true;
+        order[R] = ++count;
 
-        for(int next : l.get(node)){
+        for(int next : l.get(R)){
             if(!visited[next]){
                 dfs(next);
             }
         }
+
     }
 }
 
