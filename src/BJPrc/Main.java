9437527d1da67665022533070d7145914d96a2d6
Maoka2 <a24358232@gmail.com>
2025-03-05 22:08:04 +0900

    백준 7576 토마토 https://www.acmicpc.net/problem/7576 BFS, 한번에 여러개?

--- src/BJPrc/Main.java
+++ src/BJPrc/Main.java
@@ -1,63 +1,97 @@
 package BJPrc;
 
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int N;
-    static int[][] quad;
-    static BufferedWriter bw;
+    static int N, M;
+    static int[][] tomato;
+    static boolean[][] visited;
+    static Queue<int[]> q = new LinkedList<>();
 
     public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        N = Integer.parseInt(br.readLine());
-        quad = new int[N][N];
+        String[] nm = br.readLine().split(" ");
+        M = Integer.parseInt(nm[0]);
+        N = Integer.parseInt(nm[1]);
+
+        tomato = new int[N][M];
+        visited = new boolean[N][M];
+
+
+        boolean noTrash = true;
         for (int i = 0; i < N; i++) {
-            String s = br.readLine();
-            for (int j = 0; j < N; j++) {
-                quad[i][j] = s.charAt(j) - '0';
-            }
-        }
-
-
-        recur(0, 0, N);
-
-        bw.flush();
-        br.close();
-        bw.close();
-    }
-
-    static void recur(int x, int y, int size) throws IOException {
-        boolean isPossible = true;
-
-
-        // 반복 조건
-        int temp = quad[x][y];
-
-        a:
-        for (int i = x; i < x + size; i++) {
-            for (int j = y; j < y + size; j++) {
-                if (temp != quad[i][j]) {
-                    isPossible = false;
-                    break a;
+            String[] s = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                tomato[i][j] = Integer.parseInt(s[j]);
+                if (tomato[i][j] == 0) {
+                    noTrash = false;
+                } else if (tomato[i][j] == 1) {
+                    q.add(new int[]{i, j,0});
+                    visited[i][j] = true;
                 }
             }
         }
 
-        if (isPossible) {
-            bw.write(String.valueOf(temp));
+        if (noTrash) {
+            bw.write("0\n");
+            bw.flush();
+            br.close();
+            bw.close();
             return;
         }
 
-        bw.write('(');
+        int ans = bfs();
 
-        recur(x, y, size / 2);
-        recur(x, y + size / 2, size / 2);
-        recur(x + size / 2, y, size / 2);
-        recur(x + size / 2, y + size / 2, size / 2);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (tomato[i][j] == 0) {
+                    bw.write("-1\n");
+                    bw.flush();
+                    bw.close();
+                    br.close();
+                    return;
+                }
+            }
+        }
 
-        bw.write(')');
+        bw.write(ans + "\n");
+
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static int bfs() {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
+        int min = Integer.MIN_VALUE;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int cx = current[0];
+            int cy = current[1];
+            int day = current[2];
+
+            min = Math.max(min,day);
+
+            for (int i = 0; i < 4; i++) {
+                int mx = cx + dx[i];
+                int my = cy + dy[i];
+
+                if (mx >= 0 && my >= 0 && mx < N && my < M) {
+                    if (!visited[mx][my] && tomato[mx][my] == 0) {
+                        visited[mx][my] = true;
+                        tomato[mx][my] = 1;
+                        q.add(new int[]{mx, my, day + 1});
+                    }
+                }
+
+            }
+        }
+        return min;
     }
 }
